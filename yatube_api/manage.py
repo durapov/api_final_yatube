829028f19f608a60974deaf8f6fe53704035e13d
Dmitry Durapov <ddurapov@gmail.com>
2024-01-11 04:18:23 +0300

    2

--- yatube_api/manage.py
+++ yatube_api/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube_api.settings')
     try:
-        from django.core.management import execute_from_command_linesou 
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -4,9 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY", default="hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4')
-# SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
 
 DEBUG = True
 
